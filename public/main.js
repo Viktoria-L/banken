f73eac16edf0d0f4d97e74bd72d59b49733e708c
Viktoria Lindberg <viktorial86@hotmail.com>
2023-05-16 16:01:18 +0200

    first

---
+++ public/main.js
@@ -0,0 +1,120 @@
+const loginForm = document.querySelector("#loginOrReg-form");
+const logoutForm = document.querySelector("#logout");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const username = document.getElementById("user");
+const pass = document.getElementById("pass");
+const submitBtn = document.querySelector(".submitBtn");
+const welcomeText = document.querySelector("#welcome");
+const loginSection = document.querySelector(".loginSection");
+const changeBtn = document.querySelector(".changeBtn");
+const userSection = document.querySelector(".userSection");
+let state = "";
+
+let onPageLoad = async () => {
+    let response = await fetch('/api/loggedin');
+    let data = await response.json();
+    console.log(data)
+    if(data.user){
+        welcomeText.innerHTML = "Welcome back, " + data.user;
+        loginSection.classList.add("hidden");
+        userSection.classList.remove("hidden");
+        logoutForm.classList.remove("hidden");
+    } else {
+        state = 'login';      
+    }
+}
+onPageLoad();
+
+changeBtn.addEventListener("click", () => {
+    firstname.classList.toggle('hidden');
+    lastname.classList.toggle('hidden');
+    if (state === 'login') {
+    state = 'register';
+    firstname.required = true;
+    lastname.required = true;
+    submitBtn.innerText = "Registrera dig";
+    changeBtn.innerText = `Är du redan medlem, klicka här för att logga in!`     
+} else { 
+        state = 'login';
+        firstname.required = false;
+        lastname.required = false;
+        submitBtn.innerText = "Logga in";
+        changeBtn.innerText = `Är du inte medlem än? Klicka här för att registrera dig!`;
+    }
+})
+
+//Skapa användare eller logga in
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const usernameInput = username.value;
+    const passwordInput = pass.value;
+
+    if(state === 'login'){
+        let res = await fetch('/api/login',
+        {
+        method: "POST",
+        body: JSON.stringify({
+            user: usernameInput,
+            pass: passwordInput,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+            }
+        })
+        let data = await res.json();
+        console.log(data);
+        if(data.user){
+        loginSection.classList.add("hidden");
+        userSection.classList.remove("hidden");
+        welcomeText.innerText = "Välkommen, du är inloggad";
+        } else {
+            welcomeText.innerText = data.error;
+        }
+
+    } else if (state === 'register'){
+        const firstnameInput = firstname.value;
+        const lastnameInput = lastname.value;
+
+       let res = await fetch('/api/register',
+        {
+        method: "POST",
+        body: JSON.stringify({
+            firstname: firstnameInput,
+            lastname: lastnameInput,
+            user: usernameInput,
+            pass: passwordInput,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+            }
+        })
+
+        let data = await res.json();
+        console.log(data);
+        if(data.user){
+        loginSection.classList.add("hidden");
+        userSection.classList.remove("hidden");
+        welcomeText.innerText = "Välkommen, tack för att du har registrerat dig!";
+        } else {
+            welcomeText.innerText = "Något gick fel vid registreringen";
+        }
+    }
+})
+
+//Logga ut användare
+logoutForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    await fetch('/api/logout', 
+    {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    location.reload();
+})
+
+
+
+
---
+++ server.js
@@ -0,0 +1,124 @@
+import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import session from "express-session";
+import bcrypt from "bcrypt";
+import cookieParser from "cookie-parser";
+
+//Express setup
+const port = 3000;
+const app = express();
+const saltRounds = 10;
+
+app.use(cookieParser());
+app.use(express.static("public"));
+app.use(express.json());
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "shhhh, very secret",
+    // cookie: {
+    //  maxAge: 5 * 60 * 1000
+    // }
+  })
+);
+
+//MongoDB setup
+const client = new MongoClient("mongodb://127.0.0.1:27017");
+await client.connect();
+const db = client.db("Banken");
+const usersCollection = db.collection("users");
+const accountCollection = db.collection("accounts");
+
+// Middleware
+const restrict = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    res.status(401).send({ error: "Ej behörig" });
+  }
+};
+
+//Routes
+
+// -------- INLOGGAD-PAGELOAD ------------- //
+app.get("/api/loggedin", async (req, res) => {
+  if (req.session.user) {
+    res.json({
+      user: req.session.user,
+    });
+  } else {
+    res.status(401).json({ error: "Användare ej inloggad" });
+  }
+});
+
+// --------------- HÄMTA ALLA ANVÄNDARE --------------- //
+app.get("/api/users", async (req, res) => {
+  const users = await usersCollection.find().toArray();
+  res.json(users);
+});
+
+//--------- REGISTRERA ANVÄNDARE ------------- //
+app.post("/api/register", async (req, res) => {
+  const hash = await bcrypt.hash(req.body.pass, saltRounds);
+
+  await usersCollection.insertOne({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    user: req.body.user,
+    pass: hash,
+  });
+  res.json({
+    success: true,
+    user: req.body.user,
+  });
+});
+
+// ------------- LOGGA IN ----------------- //
+app.post("/api/login", async (req, res) => {
+  const user = await usersCollection.findOne({
+    user: req.body.user,
+  });
+  const passwordsMatch = await bcrypt.compare(req.body.pass, user.pass);
+  if (user && passwordsMatch) {
+    req.session.user = user.user;
+    res.json({
+      success: true,
+      user: user.user,
+    });
+  } else {
+    res
+      .status(401)
+      .json({ error: "Fel användarnamn eller lösenord! Försök igen." });
+  }
+});
+
+// -------- LOGGA UT ---------- //
+app.post("/api/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.json({
+      loggedin: false,
+    });
+  });
+});
+
+// ---------- SKAPA KONTO --------- //
+app.get('/aacount/add', async () => {
+
+})
+
+
+//TA BORT DENNA SEN?
+app.get("/api/cookie", (req, res) => {
+     res.send(req.session);
+    
+});
+
+
+//----------- 404 --------------- //
+app.use((req, res) => {
+  res.status(404).send("Sidan du sökte efter finns inte");
+});
+
+// -------- LISTENING ---------- //
+app.listen(port, () => console.log(`Listening to port ${port}`));
